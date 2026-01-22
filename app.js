51e9fb94ce667e0e1fb7ec86fb1f4e0860ce1111
nguyenthuc1 <nguyenthuc24122010@gmail.com>
2026-01-22 08:18:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.post("/api/task/start", async (req, res) => {
 
   res.json({
     sessionId,
-    url: "https:https://link4m.com/xHprfP/abc123" // LINK RÚT GỌN
+    url: "https://link4m.com/xHprfP" // LINK RÚT GỌN
   })
-})https
+})
