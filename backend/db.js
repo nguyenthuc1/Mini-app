d7da03cf38d24475ba7b0eee17672a159fd385d1
nguyenthuc1 <nguyenthuc24122010@gmail.com>
2026-01-31 15:50:51 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,25 @@
-const users = new Map()
-const sessions = new Map()
+var admin = require("firebase-admin");
 
-module.exports = { users, sessions }
+try {
+  // 1. Lấy chìa khóa từ Render
+  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+  // 2. [QUAN TRỌNG] Tự động sửa lỗi xuống dòng khi copy trên điện thoại
+  if (serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  }
+
+  // 3. Kết nối Firebase
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://telegram-bot-backup-11c83-default-rtdb.firebaseio.com"
+  });
+
+  console.log("✅ Kết nối Firebase thành công!");
+
+} catch (error) {
+  console.error("❌ LỖI KẾT NỐI DB: " + error.message);
+}
+
+const db = admin.database();
+module.exports = db;
